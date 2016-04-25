d4c959dcb8a054ae95c9c6d650a9e144a4b2cbb9
rayoucoy <kutilkutilun@gmail.com>
2016-04-26 04:31:28 +0700

    regact blum fix

---
+++ app/src/main/java/com/skripsi/yudha/choloc/AppConfig.java
@@ -0,0 +1,9 @@
+package com.skripsi.yudha.choloc;
+
+/**
+ * Created by yudha on 26/04/2016.
+ */
+public class AppConfig {
+
+    public static String URL_REGISTER = "http://luxarcadia.com/choloc/api/register.php";
+}
