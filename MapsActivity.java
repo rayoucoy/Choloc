bb63945918f5c4ee922fdb97508713b8345f8bcc
rayoucoy <kutilkutilun@gmail.com>
2016-05-31 01:23:12 +0700

    Add files via upload

---
+++ MapsActivity.java
@@ -0,0 +1,351 @@
+package com.skripsi.yudha.choloc;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.text.Html;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.model.LatLng;
+import com.skripsi.yudha.choloc.Tracker.GPSTracker;
+import com.skripsi.yudha.choloc.Tracker.Place;
+import com.skripsi.yudha.choloc.Tracker.PlacesList;
+import com.skripsi.yudha.choloc.helper.AlertDialogManager;
+import com.skripsi.yudha.choloc.helper.ConnectionDetector;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class MapsActivity extends AppCompatActivity {
+    static final LatLng SEMARANG = new LatLng(-7.049645, 110.438541);
+    static final LatLng BREBES = new LatLng(-6.841648,109.044968);
+//    private GoogleMap map;
+    MapView mMapView;
+    private GoogleMap googleMap;
+
+    // flag for Internet connection status
+    Boolean isInternetPresent = false;
+
+    // Connection detector class
+    ConnectionDetector cd;
+
+    // Alert Dialog Manager
+    AlertDialogManager alert = new AlertDialogManager();
+
+    // Google Places
+    //GooglePlaces googlePlaces;
+
+    // Places List
+    PlacesList nearPlaces;
+
+    // GPS Location
+    GPSTracker gps;
+
+    // Button
+    Button btnShowOnMap;
+
+    // Progress dialog
+    ProgressDialog pDialog;
+
+    // Places Listview
+    ListView lv;
+
+    // ListItems data
+    ArrayList<HashMap<String, String>> placesListItems = new ArrayList<HashMap<String,String>>();
+
+    // KEY Strings
+    public static String KEY_REFERENCE = "reference"; // id of the place
+    public static String KEY_NAME = "name"; // name of the place
+    public static String KEY_VICINITY = "vicinity"; // Place area name
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        initEvent();
+//        initMap();
+        addMapFragment();
+        cd = new ConnectionDetector(getApplicationContext());
+
+        // Check if Internet present
+        isInternetPresent = cd.isConnectingToInternet();
+        if (!isInternetPresent) {
+            // Internet Connection is not present
+            alert.showAlertDialog(MapsActivity.this, "Internet Connection Error",
+                    "Please connect to working Internet connection", false);
+            // stop executing code by return
+            return;
+        }
+
+        // creating GPS Class object
+        gps = new GPSTracker(this);
+
+        // check if GPS location can get
+        if (gps.canGetLocation()) {
+            Log.d("Your Location", "latitude:" + gps.getLatitude() + ", longitude: " + gps.getLongitude());
+        } else {
+            // Can't get user's current location
+            alert.showAlertDialog(MapsActivity.this, "GPS Status",
+                    "Couldn't get location information. Please enable GPS",
+                    false);
+            // stop executing code by return
+            return;
+        }
+
+        // Getting listview
+        lv = (ListView) findViewById(R.id.list);
+
+        // button show on map
+        btnShowOnMap = (Button) findViewById(R.id.btn_show_map);
+
+        // calling background Async task to load Google Places
+        // After getting places from Google all the data is shown in listview
+        new LoadPlaces().execute();
+
+        /** Button click event for shown on map */
+        btnShowOnMap.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                Intent i = new Intent(getApplicationContext(),
+                        PlacesMapActivity.class);
+                // Sending user current geo location
+                i.putExtra("user_latitude", Double.toString(gps.getLatitude()));
+                i.putExtra("user_longitude", Double.toString(gps.getLongitude()));
+
+                // passing near places to map activity
+                i.putExtra("near_places", nearPlaces);
+                // staring activity
+                startActivity(i);
+            }
+        });
+
+
+        /**
+         * ListItem click event
+         * On selecting a listitem SinglePlaceActivity is launched
+         * */
+
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                // getting values from selected ListItem
+                String reference = ((TextView) view.findViewById(R.id.reference)).getText().toString();
+
+                // Starting new intent
+                Intent in = new Intent(getApplicationContext(),
+                        SinglePlaceActivity.class);
+
+                // Sending place refrence id to single place activity
+                // place refrence id used to get "Place full details"
+                in.putExtra(KEY_REFERENCE, reference);
+                startActivity(in);
+            }
+        });
+    }
+
+    /**
+     * Background Async Task to Load Google places
+     * */
+
+    class LoadPlaces extends AsyncTask<String, String, String> {
+
+        /**
+         * Before starting background thread Show Progress Dialog
+         * */
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            pDialog = new ProgressDialog(MapsActivity.this);
+            pDialog.setMessage(Html.fromHtml("<b>Search</b><br/>Loading Places..."));
+            pDialog.setIndeterminate(false);
+            pDialog.setCancelable(false);
+            pDialog.show();
+        }
+
+        /**
+         * getting Places JSON
+         * */
+
+        protected String doInBackground(String... args) {
+            // creating Places class object
+            //googlePlaces = new GooglePlaces();
+
+            try {
+                // Separeate your place types by PIPE symbol "|"
+                // If you want all types places make it as null
+                // Check list of types supported by google
+                //
+                String types = "cafe|restaurant"; // Listing places only cafes, restaurants
+
+                // Radius in meters - increase this value if you don't find any places
+                double radius = 1000; // 1000 meters
+
+                // get nearest places
+                //nearPlaces = googlePlaces.search(gps.getLatitude(),
+                //        gps.getLongitude(), radius, types);
+
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        /**
+         * After completing background task Dismiss the progress dialog
+         * and show the data in UI
+         * Always use runOnUiThread(new Runnable()) to update UI from background
+         * thread, otherwise you will get error
+         * **/
+
+        protected void onPostExecute(String file_url) {
+            // dismiss the dialog after getting all products
+            pDialog.dismiss();
+            // updating UI from Background Thread
+            runOnUiThread(new Runnable() {
+                public void run() {
+                    /**
+                     * Updating parsed Places into LISTVIEW
+                     * */
+                    // Get json response status
+                    String status = nearPlaces.status;
+
+                    // Check for all possible status
+                    if(status.equals("OK")){
+                        // Successfully got places details
+                        if (nearPlaces.results != null) {
+                            // loop through each place
+                            for (Place p : nearPlaces.results) {
+                                HashMap<String, String> map = new HashMap<String, String>();
+
+                                // Place reference won't display in listview - it will be hidden
+                                // Place reference is used to get "place full details"
+                                map.put(KEY_REFERENCE, p.reference);
+
+                                // Place name
+                                map.put(KEY_NAME, p.name);
+
+
+                                // adding HashMap to ArrayList
+                                placesListItems.add(map);
+                            }
+                            // list adapter
+
+                            ListAdapter adapter = new SimpleAdapter(MapsActivity.this, placesListItems,
+                                    R.layout.list_item,
+                                    new String[] { KEY_REFERENCE, KEY_NAME}, new int[] {
+                                    R.id.reference, R.id.name });
+
+                            // Adding data into listview
+                            lv.setAdapter(adapter);
+                        }
+                    }
+                    else if(status.equals("ZERO_RESULTS")){
+                        // Zero results found
+                        alert.showAlertDialog(MapsActivity.this, "Near Places",
+                                "Sorry no places found. Try to change the types of places",
+                                false);
+                    }
+                    else if(status.equals("UNKNOWN_ERROR"))
+                    {
+                        alert.showAlertDialog(MapsActivity.this, "Places Error",
+                                "Sorry unknown error occured.",
+                                false);
+                    }
+                    else if(status.equals("OVER_QUERY_LIMIT"))
+                    {
+                        alert.showAlertDialog(MapsActivity.this, "Places Error",
+                                "Sorry query limit to google places is reached",
+                                false);
+                    }
+                    else if(status.equals("REQUEST_DENIED"))
+                    {
+                        alert.showAlertDialog(MapsActivity.this, "Places Error",
+                                "Sorry error occured. Request is denied",
+                                false);
+                    }
+                    else if(status.equals("INVALID_REQUEST"))
+                    {
+                        alert.showAlertDialog(MapsActivity.this, "Places Error",
+                                "Sorry error occured. Invalid Request",
+                                false);
+                    }
+                    else
+                    {
+                        alert.showAlertDialog(MapsActivity.this, "Places Error",
+                                "Sorry error occured.",
+                                false);
+                    }
+                }
+            });
+
+        }
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+//        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
+
+    private void addMapFragment() {
+        FragmentManager manager = getSupportFragmentManager();
+        FragmentTransaction transaction = manager.beginTransaction();
+        MapFragment fragment = new MapFragment();
+//        transaction.add(R.id.mapView, fragment);
+        transaction.commit();
+    }
+    /*private void initEvent() {
+        ImageButton imageButtonChatList = (ImageButton)findViewById(R.id.chatlist_button);
+        imageButtonChatList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i;
+                i = new Intent(MapsActivity.this,ChatListActivity.class);
+                startActivity(i);
+            }
+        });
+
+        ImageButton imageButtonFriendList = (ImageButton)findViewById(R.id.friendlist_button);
+        imageButtonFriendList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i;
+                i = new Intent(MapsActivity.this, FriendListActivity.class);
+                startActivity(i);
+            }
+        });
+
+        ImageButton imageButtonProfile = (ImageButton)findViewById(R.id.profile_button);
+        imageButtonProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i;
+                i = new Intent(MapsActivity.this, ProfileActivity.class);
+                startActivity(i);
+            }
+        });
+    } */
+
+}
+
+
+
