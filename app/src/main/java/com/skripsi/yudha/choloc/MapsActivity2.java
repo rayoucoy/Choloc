d779625324f198503425be8068e17905b8cbf9bc
paralust <yunus.herfanda@gmail.com>
2016-05-31 03:01:06 +0700

    **** Update **** implement fragment into new class and function in google maps for tracking

--- app/src/main/java/com/skripsi/yudha/choloc/MapsActivity2.java
+++ app/src/main/java/com/skripsi/yudha/choloc/MapsActivity2.java
@@ -1,26 +1,33 @@
 package com.skripsi.yudha.choloc;
 
+import android.app.Fragment;
 import android.content.Context;
 import android.content.Intent;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.ActionBarActivity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 
 /**
  * Created by yudha on 29/05/16.
  */
-public class MapsActivity2 extends ActionBarActivity implements LocationListener{
+public class MapsActivity2 extends ActionBarActivity, Fragment implements LocationListener {
     Button ceklokasi, cekdekat;
-    public String kode="kode", KEY1 = "key1", KEY2 = "key2", KEY3 = "key3", KEY4 = "key4", LAT1 = "1", LAT2 = "2", LAT3 = "3", LAT4 = "4", LONG1 = "11", LONG2 = "22", LONG3 = "33", LONG4 = "44", CURLAT = "111", CURLONG = "222";
+    public String kode = "kode", KEY1 = "key1", KEY2 = "key2", KEY3 = "key3", KEY4 = "key4", LAT1 = "1", LAT2 = "2", LAT3 = "3", LAT4 = "4", LONG1 = "11", LONG2 = "22", LONG3 = "33", LONG4 = "44", CURLAT = "111", CURLONG = "222";
     public static String satu, dua, tiga, empat, lat1, lat2, lat3, lat4, long1, long2, long3, long4;
     protected LocationManager locationManager;
     protected LocationListener locationListener;
@@ -28,13 +35,20 @@ public class MapsActivity2 extends ActionBarActivity implements LocationListener
     String provider;
     public static String curlat = "-6.973981", curlong = "107.6293685";
     protected boolean gps_enabled, network_enabled;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_maps2);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_maps2);
+        View rootView = inflater.inflate(R.layout.activity_maps2, container, false);
+
         ceklokasi = (Button) findViewById(R.id.ceklokasi);
-        cekdekat=(Button) findViewById(R.id.cekdekat);
+        cekdekat = (Button) findViewById(R.id.cekdekat);
 
         ceklokasi.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -49,12 +63,13 @@ public class MapsActivity2 extends ActionBarActivity implements LocationListener
             @Override
             public void onClick(View v) {
                 Intent in = new Intent(getApplicationContext(), MapsLoc.class);
-                in.putExtra(kode,"ALL");
+                in.putExtra(kode, "ALL");
                 in.putExtra(CURLAT, curlat);
                 in.putExtra(CURLONG, curlong);
                 startActivity(in);
             }
         });
+        return rootView;
     }
 
     private void showCurrentLocation() {
@@ -64,10 +79,10 @@ public class MapsActivity2 extends ActionBarActivity implements LocationListener
             String message = String.format(
                     "Current Location \n Longitude: %1$s \n Latitude: %2$s",
                     location.getLongitude(), location.getLatitude());
-            curlat=String.valueOf(location.getLatitude());
-            curlong=String.valueOf(location.getLongitude());
+            curlat = String.valueOf(location.getLatitude());
+            curlong = String.valueOf(location.getLongitude());
             Toast.makeText(MapsActivity2.this, message, Toast.LENGTH_LONG).show();
-        }else{
+        } else {
             Toast.makeText(MapsActivity2.this, "Lokasi tidak terdeteksi", Toast.LENGTH_LONG).show();
         }
     }
@@ -91,5 +106,54 @@ public class MapsActivity2 extends ActionBarActivity implements LocationListener
     @Override
     public void onProviderDisabled(String provider) {
 
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "MapsActivity2 Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://com.skripsi.yudha.choloc/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "MapsActivity2 Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://com.skripsi.yudha.choloc/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
     }
 }
