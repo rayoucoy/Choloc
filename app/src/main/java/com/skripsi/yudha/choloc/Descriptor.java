99734a63ea876084018b9aaed3603eddbe5f66ba
rayoucoy <kutilkutilun@gmail.com>
2016-06-17 12:57:28 +0700

    maps UDAH ADA data

--- app/src/main/java/com/skripsi/yudha/choloc/Descriptor.java
+++ app/src/main/java/com/skripsi/yudha/choloc/Descriptor.java
@@ -28,21 +28,67 @@ public class Descriptor extends AppCompatActivity {
         String a=i.getStringExtra("value");
         if (a.equalsIgnoreCase("1")){
             img.setImageResource(R.drawable.kawah_putih_ciwidey);
-            title.setText("Kawah Putih");
+            title.setText("Kampung Wisata Taman Lele");
             desc.setText(R.string.desc_kawahputih);
         }else if (a.equalsIgnoreCase("2")) {
             img.setImageResource(R.drawable.tsb);
-            title.setText("Trans Studio Bandung");
+            title.setText("Wisata Bahari Morosari");
             desc.setText(R.string.desc_tsm);
         }else if (a.equalsIgnoreCase("3")) {
             img.setImageResource(R.drawable.gedungsate);
-            title.setText("Gedung Sate");
-            desc.setText(R.string.desc_gedungsate);
+            title.setText("Wisata Goa Kreo");
         }else if (a.equalsIgnoreCase("4")) {
-            img.setImageResource(R.drawable.situ_patenggang);
-            title.setText("Situ Patenggang / Situ Patengan");
-//            desc.setText(R.string.desc_situpatenggang);
+            title.setText("Wisata Alam Wana Wisata Penggaron");
+        }else if (a.equalsIgnoreCase("5")) {
+            title.setText("Umbul Sidomukti");
+        }else if (a.equalsIgnoreCase("6")) {
+            title.setText("Vanaprastha Gedongsongo Park");
+        }else if (a.equalsIgnoreCase("7")) {
+            title.setText("Desa Wisata Lembah Kalipancur");
+        }else if (a.equalsIgnoreCase("8")) {
+            title.setText("Air Terjun Kalipancur Nogosaren");
+        }else if (a.equalsIgnoreCase("9")) {
+            title.setText("Watu Gunung");
+        }else if (a.equalsIgnoreCase("10")) {
+            title.setText("Candi Gedong Songo");
+        }else if (a.equalsIgnoreCase("11")) {
+            title.setText("Curug Lawe Benowo Kalisidi");
+        }else if (a.equalsIgnoreCase("12")) {
+            title.setText("Bukit Cinta Rawa Pening");
+        }else if (a.equalsIgnoreCase("13")) {
+            title.setText("Kolam Renang Tirto Agung Siwarak");
+        }else if (a.equalsIgnoreCase("14")) {
+            title.setText("Basecamp Mawar");
+        }else if (a.equalsIgnoreCase("15")) {
+            title.setText("Simpang Lima Semarang");
+        }else if (a.equalsIgnoreCase("16")) {
+            title.setText("Lawang Sewu");
+        }else if (a.equalsIgnoreCase("17")) {
+            title.setText("Tugu Muda");
+        }else if (a.equalsIgnoreCase("18")) {
+            title.setText("Universitas Diponegoro, Kampus Tembalang Semarang");
+        }else if (a.equalsIgnoreCase("19")) {
+            title.setText("Universitas Diponegoro, Kampus Pleburan Semarang");
+        }else if (a.equalsIgnoreCase("20")) {
+            title.setText("Universitas Negeri Semarang");
+        }else if (a.equalsIgnoreCase("21")) {
+            title.setText("Gunung Ungaran");
+        }else if (a.equalsIgnoreCase("22")) {
+            title.setText("Candi Gedong Songo");
+        }else if (a.equalsIgnoreCase("23")) {
+            title.setText("Bukit Paralayang");
+        }else if (a.equalsIgnoreCase("24")) {
+            title.setText("Pondok Kopi Umbul Sidomukti");
+        }else if (a.equalsIgnoreCase("25")) {
+            title.setText("RAGENTAR Outbound dan Olah Nyali");
+        }else if (a.equalsIgnoreCase("26")) {
+            title.setText("Browncanyon Semarang");
+        }else if (a.equalsIgnoreCase("27")) {
+            title.setText("Water Blaster");
+        }else if (a.equalsIgnoreCase("28")) {
+            title.setText("Masjid Agung Jawa Tengah");
         }
+
     }
 
 }
--- app/src/main/java/com/skripsi/yudha/choloc/MapsActivity.java
+++ app/src/main/java/com/skripsi/yudha/choloc/MapsActivity.java
@@ -27,13 +27,42 @@ import java.util.List;
  */
 public class MapsActivity extends FragmentActivity {
 
-    private static LatLng LAT1= new LatLng(-7.1661110,107.4022220);
-    private static LatLng LAT2= new LatLng(-7.1669440,107.3580560);
-    private static LatLng LAT3= new LatLng(-6.9259410,107.6361910);
-    private static LatLng LAT4= new LatLng(-6.8999250,107.6229660);
-    private static LatLng CURRENT= new LatLng(-6.973981, 107.6293685);
+    private static LatLng LAT1= new LatLng(-6.985906,110.345631);
+    private static LatLng LAT2= new LatLng(-6.924171,110.478484);
+    private static LatLng LAT3= new LatLng(-7.038525,110.351087);
+    private static LatLng LAT4= new LatLng(-7.116164,110.422420);
+    private static LatLng LAT5= new LatLng(-7.194939,110.373513);
+    private static LatLng LAT6= new LatLng(-7.205499,110.341971);
+    private static LatLng LAT7= new LatLng(-7.021035,110.376843);
+    private static LatLng LAT8= new LatLng(-7.359403,110.422859);
+    private static LatLng LAT9= new LatLng(-7.129845,110.389233);
+    private static LatLng LAT10= new LatLng(-7.208178,110.341753);
+    private static LatLng LAT11= new LatLng(-7.148884,110.360616);
+    private static LatLng LAT12= new LatLng(-7.304380,110.411302);
+    private static LatLng LAT13= new LatLng(-7.131754,110.395018);
+    private static LatLng LAT14= new LatLng(-7.192791,110.364666);
+    private static LatLng LAT15= new LatLng(-6.990378,110.423124);
+    private static LatLng LAT16= new LatLng(-6.984069,110.410814);
+    private static LatLng LAT17= new LatLng(-6.984336,110.409301);
+    private static LatLng LAT18= new LatLng(-7.049550,110.438541);
+    private static LatLng LAT19= new LatLng(-6.994744,110.424609);
+    private static LatLng LAT20= new LatLng(-7.050616,110.392740);
+    private static LatLng LAT21= new LatLng(-7.183613,110.348332);
+    private static LatLng LAT22= new LatLng(-7.208187,110.341747);
+    private static LatLng LAT23= new LatLng(-7.189797,110.365043);
+    private static LatLng LAT24= new LatLng(-7.193205,110.366087);
+    private static LatLng LAT25= new LatLng(-7.194812,110.373951);
+    private static LatLng LAT26= new LatLng(-7.056871,110.487885);
+    private static LatLng LAT27= new LatLng(-7.022884,110.438683);
+    private static LatLng LAT28= new LatLng(-6.983540,110.445196);
+
+    private static LatLng CURRENT= new LatLng(-7.048015,110.441016);
     public static String KEY1="key1",KEY2="key2",KEY3="key3",KEY4="key4",lat1="1", lat2="2",lat3="3",lat4="4",long1="11",long2="22",long3="33",long4="44",CURLAT="111",CURLONG="222";
-    public static String latsek,longsek,lokasi1,lokasi2,lokasi3,lokasi4,lati1,lati2,lati3,lati4,longi1,longi2,longi3,longi4;
+    public static String latsek,longsek,lokasi1,lokasi2,lokasi3,lokasi4,lokasi5,lokasi6,lokasi7,lokasi8,lokasi9,lokasi10,
+            lokasi11,lokasi12,lokasi13,lokasi14,lokasi15,lokasi16,lokasi17,lokasi18,lokasi19,lokasi20,
+            lokasi21,lokasi22,lokasi23,lokasi24,lokasi25,lokasi26,lokasi27,lokasi28,
+            lati1,lati2,lati3,lati4,longi1,longi2,longi3,longi4;
+
     GoogleMap googleMap;
     final String TAG = "PathGoogleMapActivity";
     SessionManagement session;
@@ -67,6 +96,30 @@ public class MapsActivity extends FragmentActivity {
         options.position(LAT2);
         options.position(LAT3);
         options.position(LAT4);
+        options.position(LAT5);
+        options.position(LAT6);
+        options.position(LAT7);
+        options.position(LAT8);
+        options.position(LAT9);
+        options.position(LAT10);
+        options.position(LAT11);
+        options.position(LAT12);
+        options.position(LAT13);
+        options.position(LAT14);
+        options.position(LAT15);
+        options.position(LAT16);
+        options.position(LAT17);
+        options.position(LAT18);
+        options.position(LAT19);
+        options.position(LAT20);
+        options.position(LAT21);
+        options.position(LAT22);
+        options.position(LAT23);
+        options.position(LAT24);
+        options.position(LAT25);
+        options.position(LAT26);
+        options.position(LAT27);
+        options.position(LAT28);
         googleMap.addMarker(options);
         String url = getMapsApiDirectionsUrl();
         ReadTask downloadTask = new ReadTask();
@@ -78,96 +131,168 @@ public class MapsActivity extends FragmentActivity {
             @Override
             public void onInfoWindowClick(Marker marker) {
                 Intent i=new Intent(getApplicationContext(), Descriptor.class);
-                if (marker.getTitle().equalsIgnoreCase("kawah putih"))
+                if (marker.getTitle().equalsIgnoreCase("Kampung Wisata Taman Lele"))
                 {
                     //handle click here
                     i.putExtra("value","1");
                     startActivity(i);
-                }else if (marker.getTitle().equalsIgnoreCase("trans studio bandung"))
+                }else if (marker.getTitle().equalsIgnoreCase("Wisata Bahari Morosari"))
                 {
                     //handle click here
                     i.putExtra("value","2");
                     startActivity(i);
-                }else if (marker.getTitle().equalsIgnoreCase("gedung sate"))
+                }else if (marker.getTitle().equalsIgnoreCase("Wisata Goa Kreo"))
                 {
                     //handle click here
                     i.putExtra("value","3");
                     startActivity(i);
-                }else if (marker.getTitle().equalsIgnoreCase("situ patengan"))
+                }else if (marker.getTitle().equalsIgnoreCase("Wisata Alam Wana Wisata Penggaron"))
                 {
                     i.putExtra("value","4");
                     startActivity(i);
                     //handle click here
-                }else if (marker.getTitle().equalsIgnoreCase("Taman Ade Irma Suryani"))
+                }else if (marker.getTitle().equalsIgnoreCase("Umbul Sidomukti"))
                 {
                     //handle click here
                     i.putExtra("value","5");
                     startActivity(i);
 
-                }else if (marker.getTitle().equalsIgnoreCase("Keraton Kasepuhan"))
+                }else if (marker.getTitle().equalsIgnoreCase("Vanaprastha Gedongsongo Park"))
                 {
                     i.putExtra("value","6");
                     startActivity(i);
 
                     //handle click here
-                }else if (marker.getTitle().equalsIgnoreCase("banyu panas palimanan"))
+                }else if (marker.getTitle().equalsIgnoreCase("Desa Wisata Lembah Kalipancur"))
                 {
                     //handle click here
                     i.putExtra("value","7");
                     startActivity(i);
 
-                }else if (marker.getTitle().equalsIgnoreCase("pantai kejawanan"))
+                }else if (marker.getTitle().equalsIgnoreCase("Air Terjun Kalipancur Nogosaren"))
                 {
                     //handle click here
                     i.putExtra("value","8");
                     startActivity(i);
 
-                }else if (marker.getTitle().equalsIgnoreCase("goa bumiayu"))
+                }else if (marker.getTitle().equalsIgnoreCase("Watu Gunung"))
                 {
                     //handle click here
                     i.putExtra("value","9");
                     startActivity(i);
 
-                }else if (marker.getTitle().equalsIgnoreCase("situ gunung"))
+                }else if (marker.getTitle().equalsIgnoreCase("Candi Gedong Songo"))
                 {
                     //handle click here
                     i.putExtra("value","10");
                     startActivity(i);
 
-                }else if (marker.getTitle().equalsIgnoreCase("situ sukarame"))
+                }else if (marker.getTitle().equalsIgnoreCase("Curug Lawe Benowo Kalisidi"))
                 {
                     //handle click here
                     i.putExtra("value","11");
                     startActivity(i);
 
-                }else if (marker.getTitle().equalsIgnoreCase("taman rekreasi selabintana"))
+                }else if (marker.getTitle().equalsIgnoreCase("Bukit Cinta Rawa Pening"))
                 {
                     //handle click here
                     i.putExtra("value","12");
                     startActivity(i);
 
-                }else if (marker.getTitle().equalsIgnoreCase("Kebun Raya Bogor"))
+                }else if (marker.getTitle().equalsIgnoreCase("Kolam Renang Tirto Agung Siwarak"))
                 {
                     //handle click here
                     i.putExtra("value","13");
                     startActivity(i);
 
-                }else if (marker.getTitle().equalsIgnoreCase("Danau Situ Gede"))
+                }else if (marker.getTitle().equalsIgnoreCase("Basecamp Mawar"))
                 {
                     //handle click here
                     i.putExtra("value","14");
                     startActivity(i);
 
-                }else if (marker.getTitle().equalsIgnoreCase("Taman Safari Bogor"))
+                }else if (marker.getTitle().equalsIgnoreCase("Simpang Lima Semarang"))
                 {
                     //handle click here
                     i.putExtra("value","15");
                     startActivity(i);
 
-                }else if (marker.getTitle().equalsIgnoreCase("Taman Wisata Mekarsari"))
+                }else if (marker.getTitle().equalsIgnoreCase("Lawang Sewu"))
                 {
                     //handle click here
                     i.putExtra("value","16");
+                    startActivity(i);
+
+                }else if (marker.getTitle().equalsIgnoreCase("Tugu Muda"))
+                {
+                    //handle click here
+                    i.putExtra("value","17");
+                    startActivity(i);
+
+                }else if (marker.getTitle().equalsIgnoreCase("Universitas Diponegoro, Kampus Tembalang Semarang"))
+                {
+                    //handle click here
+                    i.putExtra("value","18");
+                    startActivity(i);
+
+                }else if (marker.getTitle().equalsIgnoreCase("Universitas Diponegoro, Kampus Pleburan Semarang"))
+                {
+                    //handle click here
+                    i.putExtra("value","19");
+                    startActivity(i);
+
+                }else if (marker.getTitle().equalsIgnoreCase("Universitas Negeri Semarang"))
+                {
+                    //handle click here
+                    i.putExtra("value","20");
+                    startActivity(i);
+
+                }else if (marker.getTitle().equalsIgnoreCase("Gunung Ungaran"))
+                {
+                    //handle click here
+                    i.putExtra("value","21");
+                    startActivity(i);
+
+                }else if (marker.getTitle().equalsIgnoreCase("Candi Gedong Songo"))
+                {
+                    //handle click here
+                    i.putExtra("value","22");
+                    startActivity(i);
+
+                }else if (marker.getTitle().equalsIgnoreCase("Bukit Paralayang"))
+                {
+                    //handle click here
+                    i.putExtra("value","23");
+                    startActivity(i);
+
+                }else if (marker.getTitle().equalsIgnoreCase("Pondok Kopi Umbul Sidomukti"))
+                {
+                    //handle click here
+                    i.putExtra("value","24");
+                    startActivity(i);
+
+                }else if (marker.getTitle().equalsIgnoreCase("RAGENTAR Outbound dan Olah Nyali"))
+                {
+                    //handle click here
+                    i.putExtra("value","25");
+                    startActivity(i);
+
+                }else if (marker.getTitle().equalsIgnoreCase("Browncanyon Semarang"))
+                {
+                    //handle click here
+                    i.putExtra("value","26");
+                    startActivity(i);
+
+                }else if (marker.getTitle().equalsIgnoreCase("Water Blaster"))
+                {
+                    //handle click here
+                    i.putExtra("value","27");
+                    startActivity(i);
+
+                }else if (marker.getTitle().equalsIgnoreCase("Masjid Agung Jawa Tengah"))
+                {
+                    //handle click here
+                    i.putExtra("value","28");
                     startActivity(i);
 
                 }
@@ -189,13 +314,61 @@ public class MapsActivity extends FragmentActivity {
                     .title("Current Location")
                     .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)));
             googleMap.addMarker(new MarkerOptions().position(LAT1)
-                    .title(lokasi1));
+                    .title("Kampung Wisata Taman Lele"));
             googleMap.addMarker(new MarkerOptions().position(LAT2)
-                    .title(lokasi2));
+                    .title("Wisata Bahari Morosari"));
             googleMap.addMarker(new MarkerOptions().position(LAT3)
-                    .title(lokasi3));
+                    .title("Wisata Goa Kreo"));
             googleMap.addMarker(new MarkerOptions().position(LAT4)
-                    .title(lokasi4));
+                    .title("Wisata Alam Wana Wisata Penggaron"));
+            googleMap.addMarker(new MarkerOptions().position(LAT5)
+                    .title("Umbul Sidomukti"));
+            googleMap.addMarker(new MarkerOptions().position(LAT6)
+                    .title("Vanaprastha Gedongsongo Park"));
+            googleMap.addMarker(new MarkerOptions().position(LAT7)
+                    .title("Desa Wisata Lembah Kalipancur"));
+            googleMap.addMarker(new MarkerOptions().position(LAT8)
+                    .title("Air Terjun Kalipancur Nogosaren"));
+            googleMap.addMarker(new MarkerOptions().position(LAT9)
+                    .title("Watu Gunung"));
+            googleMap.addMarker(new MarkerOptions().position(LAT10)
+                    .title("Candi Gedong Songo"));
+            googleMap.addMarker(new MarkerOptions().position(LAT11)
+                    .title("Curug Lawe Benowo Kalisidi"));
+            googleMap.addMarker(new MarkerOptions().position(LAT12)
+                    .title("Bukit Cinta Rawa Pening"));
+            googleMap.addMarker(new MarkerOptions().position(LAT13)
+                    .title("Kolam Renang Tirto Agung Siwarak"));
+            googleMap.addMarker(new MarkerOptions().position(LAT14)
+                    .title("Basecamp Mawar"));
+            googleMap.addMarker(new MarkerOptions().position(LAT15)
+                    .title("Simpang Lima Semarang"));
+            googleMap.addMarker(new MarkerOptions().position(LAT16)
+                    .title("Lawang Sewu"));
+            googleMap.addMarker(new MarkerOptions().position(LAT17)
+                    .title("Tugu Muda"));
+            googleMap.addMarker(new MarkerOptions().position(LAT18)
+                    .title("Universitas Diponegoro, Kampus Tembalang Semarang"));
+            googleMap.addMarker(new MarkerOptions().position(LAT19)
+                    .title("Universitas Diponegoro, Kampus Pleburan Semarang"));
+            googleMap.addMarker(new MarkerOptions().position(LAT20)
+                    .title("Universitas Negeri Semarang"));
+            googleMap.addMarker(new MarkerOptions().position(LAT21)
+                    .title("Gunung Ungaran"));
+            googleMap.addMarker(new MarkerOptions().position(LAT22)
+                    .title("Candi Gedong Songo"));
+            googleMap.addMarker(new MarkerOptions().position(LAT23)
+                    .title("Bukit Paralayang"));
+            googleMap.addMarker(new MarkerOptions().position(LAT24)
+                    .title("Pondok Kopi Umbul Sidomukti"));
+            googleMap.addMarker(new MarkerOptions().position(LAT25)
+                    .title("RAGENTAR Outbound dan Olah Nyali"));
+            googleMap.addMarker(new MarkerOptions().position(LAT26)
+                    .title("Browncanyon Semarang"));
+            googleMap.addMarker(new MarkerOptions().position(LAT27)
+                    .title("Water Blaster"));
+            googleMap.addMarker(new MarkerOptions().position(LAT28)
+                    .title("Masjid Agung Jawa Tengah"));
         }
     }
 
